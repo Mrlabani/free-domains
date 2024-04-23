f727f947ad9f9a31c55c577830465506394ea811
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-04-23 16:11:38 +0530

    Add files via upload

---
+++ domains/lbni.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'A Web App for sharing code lyrics',
+  domain: 'is-an.app',
+  subdomain: 'lbni',
+  owner: {
+    email: 'noob.devs@outlook.com',
+    repo: ''
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
