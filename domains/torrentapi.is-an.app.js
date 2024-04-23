4cd524e91fdf3183f6dff88df9fa773871645318
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-04-23 15:26:22 +0530

    Add files via upload

---
+++ domains/torrentapi.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: "torrent api",
+  domain: "is-an.app",
+  subdomain: "torrentapi",
+  owner: {
+    repo: "",
+    email: "noob.devs@outlook.com",
+  },
+  record: {
+    CNAME: "torrent-api-py-ftbc.onrender.com",
+  },
+  proxy: false,
+});
