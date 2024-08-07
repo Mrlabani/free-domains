23ad4cc87e50f606dd464055c99be5e8fa344fc6
Labani Behera 🎯 <158763270+Mrlabani@users.noreply.github.com>
2024-08-07 10:59:04 +0530

    Create vide0.is-an.app.js

---
+++ domains/vide0.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+	description: 'Video downloader Web',
+	domain: 'is-an.app',
+	subdomain: 'vide0',
+	owner: {
+		repo: 'https://github.com/Mrlabani/video-downloader',
+		email: 'abcdefgh96684@gmail.com',
+	},
+	record: {
+		CNAME: 'cname.vercel-dns.com'
+	},
+	proxy: true,
+})
